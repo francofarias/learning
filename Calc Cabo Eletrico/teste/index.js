3af89e7cc0ca6d7d08c26fd9a1388052ae8399e8
francofarias <francofarias@outlook.com>
2021-05-07 00:44:18 -0300

    Criação do popup de cadastro de projetos

--- Calc Cabo Eletrico/teste/index.js
+++ Calc Cabo Eletrico/teste/index.js
@@ -1,3 +1,5 @@
+const app = document.getElementById('app')
+
 const conteudo = document.getElementsByClassName('conteudo')[0]
 console.log(conteudo.innerHTML)
 
@@ -7,3 +9,40 @@ btnHome.onmouseleave = () => console.log('saiu')//menuPrincSelecionar(btnHome)
 btnHome.onmouseenter = () => console.log('entrou')
 
 console.log(btnHome)
+
+
+const popupProjetoTemp = `
+<div class="popup" id="popupProjeto">
+<div class="popup__janela">
+<div class="formulario" id="formProjeto">
+<legend>Criar Novo Projeto</legend>
+<hr>
+<label for="idProjeto">Código do projeto: </label>
+<input type="text" name="idProjeto" id="idProjeto"class="formulario__textInput" placeholder="Código/id. do projeto"autofocus maxlength="20">
+<br>
+<label for="nomeProjeto">Nomo do projeto: </label>
+<input type="text" name="nomeProjeto" id="nomeProjeto"class="formulario__textInput" placeholder="Digite o nome doprojeto">
+<br>
+<label for="descricaoProjeto">Descrição do projeto: </label>
+<textarea name="descricaoProjeto" id="descricaoProjeto" class="formulario__textInput" placeholder="Digite uma breve descrição..." cols="30" rows="3" maxlength="90"></textarea>
+<br>
+<button type="submit" name="idProjeto" id="idProjeto" class="card__btn" onclick="removePopup('popupProjeto')">Criar projeto</button>
+</div>
+</div>
+`
+
+//Componente popup
+
+
+function abrePopup(props){
+    console.log(props)
+    app.innerHTML += props
+}
+function removePopup(props){
+    const popup = document.getElementById(props)
+    while(popup.firstChild){
+        popup.removeChild(popup.firstChild)
+    }
+    console.log(popup.parentNode)
+    popup.parentNode.removeChild(popup)
+}
